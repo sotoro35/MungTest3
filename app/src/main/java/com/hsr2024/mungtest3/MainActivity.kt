aa1eef798a0921d95bbe35edb95e696836dddf70
sotoro35 <sotoro35@gmail.com>
2024-04-08 10:26:22 +0900

    Initial commit

---
+++ app/src/main/java/com/hsr2024/mungtest3/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.hsr2024.mungtest3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.hsr2024.mungtest3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
